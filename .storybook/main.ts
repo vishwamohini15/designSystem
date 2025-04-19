67285e24d1b7a367544c818dd05615f0228625ab
vishwamohini15 <vishwamohini0015@gmail.com>
2025-04-19 20:59:09 +0530

    storybook-static

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,23 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
     "@storybook/addon-docs"
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: ["../public"]
 };
+
 export default config;
